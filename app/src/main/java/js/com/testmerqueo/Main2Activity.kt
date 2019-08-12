8ba06479f1b36f873dfba133ee5be41e9a51da97
Juan Vanegas <jvanegasr@famisanar.com.co>
2019-08-12 15:21:15 -0500

    cartActivity

--- app/src/main/java/js/com/testmerqueo/Main2Activity.kt
+++ app/src/main/java/js/com/testmerqueo/Main2Activity.kt
@@ -1,17 +1,27 @@
 package js.com.testmerqueo
 
 import android.os.Bundle
+import android.os.Parcelable
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
+import js.com.testmerqueo.models.MovieOb
 
 import kotlinx.android.synthetic.main.activity_main2.*
 
 class Main2Activity : AppCompatActivity() {
 
+    var listCart:List<MovieOb> = ArrayList<MovieOb>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
         setSupportActionBar(toolbar)
+
+        listCart=this.intent.getParcelableArrayListExtra("extraextra")
+        System.out.println("LISTAAAAA SIZE"+ listCart.size)
+
+        for (item in listCart) System.out.println("LISTAAAAA"+ item.getTitle())
+
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
